243c1a7c77d379cd8d8f047312087e7c1c829aac
Sergii Krymets <skrymets@intropro.com>
2018-05-11 18:10:33 +0300

    Add Electronic Queue Project scaffolding

---
+++ eque-node/src/main/java/equeue/EqueueApplication.java
@@ -0,0 +1,12 @@
+package equeue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EqueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EqueueApplication.class, args);
+	}
+}
